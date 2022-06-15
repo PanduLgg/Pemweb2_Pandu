9c72d196d64a05cce3ea738ee68e8ffdcd66724f
Linggar_1 <91925605+PanduLgg@users.noreply.github.com>
2022-06-15 15:10:18 +0700

    Praktikum 6
    membuat controller dan model dosen & matkul

---
+++ Praktikum_6/application/controllers/Dashboard.php
@@ -0,0 +1,18 @@
+<?php
+
+    class Dashboard extends CI_Controller {
+        
+        public function index() {
+            
+            $this->load->view('template/navbar');
+
+            $this->load->view('template/sidebar');
+
+            $this->load->view('dashboard');
+
+            $this->load->view('template/footer');
+
+        }
+
+    }
+?>
